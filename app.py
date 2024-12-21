773bb1ffb6220f7d76c5eda036fff6e9a2959658
Shivansh <118342696+TheShivansh-dev@users.noreply.github.com>
2024-12-21 13:51:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ if __name__ == "__main__":
    app.run(host='0.0.0.0', port=port)
    try:
         asyncio.get_event_loop().run_forever()
-    except KeyboardInterrupt:
+   except :
         print("App interrupted!")
