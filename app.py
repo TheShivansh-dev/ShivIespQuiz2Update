24a7734678bed4dbfa1d27503f0f8f5adac59893
Shivansh <118342696+TheShivansh-dev@users.noreply.github.com>
2024-12-21 13:38:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def helloworld():
     return "grey word"
 
 if __name__ == "__main__":
-   port = int(os.environ.get('PORT', 10000))
+   port = int(os.environ.get('PORT', 8080))
    app.run(host='0.0.0.0', port=port)
    
